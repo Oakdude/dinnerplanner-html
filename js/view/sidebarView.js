22ddcc90d642c9d1db3d81ea0844fbb18a705de5
Hannes Ekman <hannesek@kth.se>
2019-01-27 12:58:10 +0100

    Update sidebarView.js

--- js/view/sidebarView.js
+++ js/view/sidebarView.js
@@ -1,18 +1,18 @@
 
 var SidebarView = function (container, model) {
 
-	var numberOfGuests = container.find("#test111");
+	
 	var addedDishes = container.find("#sidebarTable");
 	var cost = container.find("#sidebarTotalCost");
 	var numGuests = container.find("#numberOfGuests");
 	var people = container.find("#people");
 
-
+	var a = model.getNumberOfGuests();
 	var dishesArr = model.getMenuNameAndCost();
 	var tCost = 0;
 	for(dish of dishesArr){
-		tCost += dish[1];
-		addedDishes.append("<tr><th>" + dish[0] + "</th>" + "<th>" + dish[1] + "</th></tr>");
+		tCost += dish[1]*a;
+		addedDishes.append("<tr><th>" + dish[0] + "</th>" + "<th>" + dish[1]*a + "</th></tr>");
 	}
 	cost.append("SEK " + tCost);
 	b = model.getNumberOfGuests();
